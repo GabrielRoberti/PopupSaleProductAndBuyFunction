bc519fd6cfac4eeb4c611a06c2dd9adcfc954f41
Gabriel Roberti <gabrielroberti0503@hotmail.com>
2024-08-24 01:29:48 -0300

    Ajuste no layout, Criaçao do modal

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 
 let cartCount = 0;
+let addBtn = 0;
 
-document.getElementById('addToCartBtn').addEventListener('click', function() {
-    cartCount++;
-    document.getElementById('cartCount').textContent = cartCount;
-});
+    document.getElementById('addToCartBtn').addEventListener('click', function() {
+        var quantitySpan = document.querySelector('.quantityItem');
+        var quantityValue = quantitySpan.textContent;
+        var modalContent = document.querySelector('.modal-content');
+        var noProductShop = document.getElementById('noProductShop');
+
+        if(quantityValue > 0){
+            cartCount += parseInt(quantityValue);
+            document.getElementById('productValueModal').textContent = cartCount;
+            document.getElementById('cartCount').textContent = cartCount;   
+            modalContent.style.display = 'block';
+            noProductShop.style.display = 'none';
+            
+        }else{
+            alert('Please select quantity');
+        }
+        
+    });
+
+    document.getElementById('btnMinus').addEventListener('click', function() {
+        
+        var quantitySpan = document.querySelector('.quantityItem');
+        var quantityValue = quantitySpan.textContent;
+        
+        if(quantityValue > 1){
+            quantityValue--;
+            quantitySpan.textContent = quantityValue;
+        }
+    });
+
+    document.getElementById('btnPlus').addEventListener('click', function() {
+      
+        var quantitySpan = document.querySelector('.quantityItem');
+        var quantityValue = quantitySpan.textContent;
+        
+        quantityValue++;
+        quantitySpan.textContent = quantityValue;
+      
+    });
 
     const carouselImages = document.querySelector('.carousel-images');
     const carouselControls = document.getElementById('carouselControls');
@@ -12,21 +48,21 @@ document.getElementById('addToCartBtn').addEventListener('click', function() {
 
     let currentIndex = 0;
 
-    images.forEach((_, index) => {
+    images.forEach((image, index) => {
 
-        const dot = document.createElement('button');
-        dot.classList.add('carousel-btn');
+        const btnEl = document.createElement('button');
+        btnEl.classList.add('carousel-btn');
 
         if (index === currentIndex) {
-            dot.classList.add('active');
+            btnEl.classList.add('active');
         }
 
-        dot.addEventListener('click', () => {
+        btnEl.addEventListener('click', () => {
             currentIndex = index;
             updateCarousel();
         });
 
-        carouselControls.appendChild(dot);
+        carouselControls.appendChild(btnEl);
     });
 
     function updateCarousel() {
@@ -34,7 +70,24 @@ document.getElementById('addToCartBtn').addEventListener('click', function() {
 
         carouselImages.style.transform = `translateX(${offset}%)`;
         
-        document.querySelectorAll('.carousel-btn').forEach((dot, index) => {
-            dot.classList.toggle('active', index === currentIndex);
+        document.querySelectorAll('.carousel-btn').forEach((btnEl, index) => {
+            btnEl.classList.toggle('active', index === currentIndex);
         });
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const cartBtn = document.getElementById('cartBtn');
+        const modal = document.getElementById('modal');
+    
+        cartBtn.addEventListener('click', function () {
+            modal.style.display = 'block';
+        });
+    
+        window.addEventListener('click', function (event) {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+
+    
